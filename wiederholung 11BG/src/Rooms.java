1471f9a9f0cdd158bcff0d9c85fcd0f768c0222e
jan pink <02-schablone.depots@icloud.com>
2023-09-08 09:04:34 +0200

    add: Rooms Class

---
+++ wiederholung 11BG/src/Rooms.java
@@ -0,0 +1,39 @@
+public class Rooms {
+
+        private static String name;
+        private static int count;
+        private static boolean hasSink;
+
+        public Rooms(String name, int count, boolean hasSink) {
+            this.name = name;
+            this.count = count;
+            this.hasSink = hasSink;
+            System.out.println("Room name: " + name + "\n" + "Room count: " + count + "\n" + "Room has sink: " + hasSink);
+        }
+
+
+
+    public static String getName() {
+        return name;
+    }
+
+    public static void setName(String name) {
+        Rooms.name = name;
+    }
+
+    public static int getCount() {
+        return count;
+    }
+
+    public static void setCount(int count) {
+        Rooms.count = count;
+    }
+
+    public static boolean isHasSink() {
+        return hasSink;
+    }
+
+    public static void setHasSink(boolean hasSink) {
+        Rooms.hasSink = hasSink;
+    }
+}
