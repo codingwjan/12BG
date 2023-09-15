c9217e0b31c19608059619f21d056b1f3191b6f5
LupusZenyx <107887860+LupusZenyx@users.noreply.github.com>
2023-09-15 11:31:13 +0200

    Delete wiederholung 11BG directory
    redesign in progress

--- wiederholung 11BG/src/RoomManagement.java
+++
@@ -1,8 +0,0 @@
-public class RoomManagement {
-
-    RoomManagement() {
-        ComputerRoom room = new ComputerRoom("Raum211", 10, true, 20);
-
-        System.out.println("ComputerRoom: "+room.getName()+ " - "+room.getSeatCount()+ " - "+room.getSeatCount()+ " - "+room.getComputerCount());
-    }
-}
