db53535e10ce26751f63a349ce6ac86559cdac66
LupusZenyx <107887860+LupusZenyx@users.noreply.github.com>
2023-09-15 11:32:52 +0200

    Add files via upload

---
+++ JavaJungle/wiederholungen 11BG W2/src/Employee.java
@@ -0,0 +1,13 @@
+public class Employee {
+    int ID;
+    String firstName;
+    String lastName;
+    String birthDate;
+
+    public Employee(int ID, String firstName, String lastName, String birthDate) {
+        this.ID = ID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+    }
+}
---
+++ JavaJungle/wiederholungen 11BG W2/src/EmploymentContract.java
@@ -0,0 +1,11 @@
+public class EmploymentContract {
+    int employeeID;
+    String jobDescription;
+    double monthlySalary;
+
+    public EmploymentContract(int employeeID, String jobDescription, double monthlySalary) {
+        this.employeeID = employeeID;
+        this.jobDescription = jobDescription;
+        this.monthlySalary = monthlySalary;
+    }
+}
